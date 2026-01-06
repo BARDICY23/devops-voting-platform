c6c76f05effa90d0fd8576ce86c2907a86af64f3
BARDICY23 <ahmedelbardicy18@gmail.com>
2026-01-07 01:11:08 +0200

    default commit

--- services/seed-data/generate-votes.sh
+++ services/seed-data/generate-votes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ab -n 1000 -c 50 -p posta http://vote/
-ab -n 1000 -c 50 -p postb http://vote/
-ab -n 1000 -c 50 -p posta http://vote/
-
+# create 3000 votes (2000 for option a, 1000 for option b)
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
+ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
