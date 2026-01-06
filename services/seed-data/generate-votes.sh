86eeef50b1a3dc2d7ad4ffbb1050f0383686fa8a
BARDICY23 <ahmedelbardicy18@gmail.com>
2026-01-07 01:29:49 +0200

    default commit

--- services/seed-data/generate-votes.sh
+++ services/seed-data/generate-votes.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+set -e
 
-# create 3000 votes (2000 for option a, 1000 for option b)
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
-ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
+echo "Seeding votes (low concurrency)..."
+
+ab -n 100 -c 5 -p posta -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
+ab -n 100 -c 5 -p postb -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
+ab -n 100 -c 5 -p posta -T "application/x-www-form-urlencoded" http://vote.voting.svc.cluster.local/
+
+echo "Done seeding"
+
