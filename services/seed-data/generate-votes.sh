731f5535b7cd495067f84ad29292fee39f3dfa2f
BARDICY23 <ahmedelbardicy18@gmail.com>
2026-01-07 00:32:07 +0200

    default commit

--- services/seed-data/generate-votes.sh
+++ services/seed-data/generate-votes.sh
@@ -1,3 +1,6 @@
-ab -n 1000 -c 50 -p posta http://voting-app-vote/
-ab -n 1000 -c 50 -p postb http://voting-app-vote/
-ab -n 1000 -c 50 -p posta http://voting-app-vote/
+#!/bin/sh
+
+ab -n 1000 -c 50 -p posta http://vote/
+ab -n 1000 -c 50 -p postb http://vote/
+ab -n 1000 -c 50 -p posta http://vote/
+
